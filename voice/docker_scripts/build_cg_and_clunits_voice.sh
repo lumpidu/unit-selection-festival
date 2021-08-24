a02f226cfa0103cffc88968884a022809caadf26
Gunnar Thor <ornolfsson@gmail.com>
2021-08-24 18:19:37 +0000

    Fix mistake in navigating docker container

--- voice/docker_scripts/build_cg_and_clunits_voice.sh
+++ voice/docker_scripts/build_cg_and_clunits_voice.sh
@@ -2,12 +2,11 @@
 
 voice_directory=$1 # e.g. /usr/local/src/voice_alfur
 
+cd $voice_directory/builds
 
 # Find the most recent build dir - grep pattern matches timestamp, tail picks most recent entry
 timestamp=`ls -ltr | grep -P "\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}"  | awk '{print$9}' | tail -1`
 cd $timestamp
-
-cd $voice_directory/builds/$timestamp
 # Do the thing
 ./bin/do_build build_prompts
 ./bin/do_build label
@@ -16,7 +15,7 @@ cd $voice_directory/builds/$timestamp
 
 
 mkdir -p $voice_directory/builds_clunits/$timestamp
-cd $voice_directory && cp -r $voice_directory/builds/$timestamp $voice_directory/builds_clunits/$timestamp
+cd $voice_directory && cp -r $voice_directory/builds/$timestamp/* $voice_directory/builds_clunits/$timestamp
 # cd $voice_directory/voice_clunits
 
 # # Finish off the clunits model
