2de7809781eac83efdd0ef0bae5f5ca32a967a6a
Gunnar Thor Örnólfsson <ornolfsson@gmail.com>
2021-11-21 20:05:28 +0000

    fix one-liner resulting in non-zero exit code

--- voice/docker_scripts/install_sptk.sh
+++ voice/docker_scripts/install_sptk.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-cd /usr/local/src
-export ESTDIR=`pwd`/speech_tools
-export FLITEDIR=`pwd`/flite
-export FESTVOXDIR=`pwd`/festvox
-export SPTKDIR=`pwd`/SPTK
-wget http://festvox.org/packed/SPTK-3.6.tar.gz
-tar zxvf SPTK-3.6.tar.gz
-mkdir SPTK
-patch -p0 <festvox/src/clustergen/SPTK-3.6.patch 
-cd SPTK-3.6
-./configure --prefix=$SPTKDIR
-make
-make install
+if [ -d /usr/local/src/SPTK-3.6]
+then exit 0
+else
+    cd /usr/local/src
+    export ESTDIR=`pwd`/speech_tools
+    export FLITEDIR=`pwd`/flite
+    export FESTVOXDIR=`pwd`/festvox
+    export SPTKDIR=`pwd`/SPTK
+    wget http://festvox.org/packed/SPTK-3.6.tar.gz
+    tar zxvf SPTK-3.6.tar.gz
+    mkdir SPTK
+    patch -p0 <festvox/src/clustergen/SPTK-3.6.patch 
+    cd SPTK-3.6
+    ./configure --prefix=$SPTKDIR
+    make
+    make install
+fi
