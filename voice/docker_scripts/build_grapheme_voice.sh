6db6a6064369b13095896d83dd8e2826991e3f7c
Gunnar Thor Örnólfsson <ornolfsson@gmail.com>
2021-12-08 23:01:39 +0000

    actually fixed the mistake

--- voice/docker_scripts/build_grapheme_voice.sh
+++ voice/docker_scripts/build_grapheme_voice.sh
@@ -5,6 +5,8 @@ voice_directory=$1 # e.g. /usr/local/src/voice_alfur
 
 cd $voice_directory/builds_grapheme
 
+voice_id=`basename $voice_directory`
+
 timestamp=`ls -ltr | grep -P "\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}"  | awk '{print$9}' | tail -1`
 cd $timestamp
 
@@ -12,20 +14,20 @@ ESTDIR=/usr/local/src/speech_tools
 FESTVOXDIR=/usr/local/src/festvox
 SPTKDIR=/usr/local/src/SPTK
 
-# $FESTVOXDIR/src/grapheme/make_cg_grapheme
+$FESTVOXDIR/src/grapheme/make_cg_grapheme
 
-# # Now you can build a voice as before. Firsty build the prompts and label the data.
-# ./bin/do_build build_prompts etc/txt.done.data
-# ./bin/do_build label etc/txt.done.data
-# ./bin/do_clustergen parallel build_utts etc/txt.done.data
-# ./bin/do_clustergen generate_statenames
-# ./bin/do_clustergen generate_filters
-# # Then do feature extraction
-# ./bin/do_clustergen parallel f0_v_sptk
-# # ./bin/do_clustergen parallel f0
-# ./bin/do_clustergen parallel mcep_sptk
-# # ./bin/do_clustergen parallel mcep
-# ./bin/do_clustergen parallel combine_coeffs_v
+# Now you can build a voice as before. Firsty build the prompts and label the data.
+./bin/do_build build_prompts etc/txt.done.data
+./bin/do_build label etc/txt.done.data
+./bin/do_clustergen parallel build_utts etc/txt.done.data
+./bin/do_clustergen generate_statenames
+./bin/do_clustergen generate_filters
+# Then do feature extraction
+./bin/do_clustergen parallel f0_v_sptk
+# ./bin/do_clustergen parallel f0
+./bin/do_clustergen parallel mcep_sptk
+# ./bin/do_clustergen parallel mcep
+./bin/do_clustergen parallel combine_coeffs_v
 # Build the models
 ./bin/traintest etc/txt.done.data
 ./bin/do_clustergen parallel cluster etc/txt.done.data.train
@@ -34,3 +36,6 @@ SPTKDIR=/usr/local/src/SPTK
 # # And generate some test examples, the first to give MCD and F0D objective measures, the second to generate standard tts output
 ./bin/do_clustergen cg_test resynth cgp etc/txt.done.data.test
 ./bin/do_clustergen cg_test tts tts etc/txt.done.data.test
+
+mkdir -p /outputs/$voice_id/grapheme/
+cp -r test/tts/*.wav /outputs/$voice_id/grapheme/
